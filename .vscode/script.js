e33118d7b0d279720aff3f5608b98b4405a0e987
Karol Santana <karol.santana@arapiraca.ufal.br>
2024-04-24 10:58:06 -0300

    Adicionados arquivos de configuração e assets iniciais para o projeto

---
+++ .vscode/script.js
@@ -0,0 +1,22 @@
+function toggleMode(){
+  const html = document.documentElement
+  html.classList.toggle("light")
+   //if(html.classList.contains("light")){
+    //html.classList.remove("light")
+  //}
+  //else {
+    //html.classList.add("light")
+  //}
+  
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    alt.setAttribute("erro2")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    alt.setAttribute("erro")
+  }
+
+ 
+}
